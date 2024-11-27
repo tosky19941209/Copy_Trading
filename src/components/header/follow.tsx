052a4f8297e84de71d3b48935c2ccc9310d53c48
tosky19941209 <felixeliass1994@gmail.com>
2024-11-27 04:54:58 -0500

    feat: facebook is added on follow component

--- src/components/header/follow.tsx
+++ src/components/header/follow.tsx
@@ -1,17 +1,17 @@
-// import YouTubeImg from "../../assets/follow/youtube-color-svgrepo-com.svg"
-// import TwitterImg from "../../assets/follow/twitter-svgrepo-com.svg"
+import YouTubeImg from "../../assets/follow/youtube-color-svgrepo-com.svg"
+import TwitterImg from "../../assets/follow/twitter-svgrepo-com.svg"
 // import TelegramImg from "../../assets/follow/telegram-svgrepo-com.svg"
 import FaceBookImg from "../../assets/follow/facebook_Logo.svg"
 const Follow = () => {
-    // const ImgList = [YouTubeImg, TwitterImg, TelegramImg]
-    const ImgList = [FaceBookImg]
+    const ImgList = [YouTubeImg, TwitterImg, FaceBookImg]
+    // const ImgList = [FaceBookImg]
     return (
-        <div className="flex gap-5 justify-center items-center border rounded-[30px] p-2 border-2   ">
+        <div className="flex gap-3 md:gap-5 justify-center items-center border rounded-[30px] p-2 border-2   ">
             {
                 ImgList.map((item: string, idx: number) => (
                     <img
                         src={item}
-                        className="w-10"
+                        className="md:w-10 w-7"
                         key={idx}
                     />
                 ))
